3616ffa5e58af9da64fe727cf25f6e5f99f1ffe2
Marcel Menke <marcel.menke1981@gmail.com>
2025-06-25 11:34:04 +0200

    Add MenuBuilderComponent scaffold
    Introduces the MenuBuilderComponent with initial HTML, SCSS, TypeScript, and a basic test. This sets up the structure for future menu builder functionality.

---
+++ src/app/components/menu-builder/menu-builder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-builder',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-builder.component.html',
+  styleUrl: './menu-builder.component.scss'
+})
+export class MenuBuilderComponent {
+
+}
