ecfc8ba7076681597f1d647509cd77b6bb273b4b
Marcel Menke <marcel.menke1981@gmail.com>
2025-06-25 11:33:53 +0200

    Add MenuPreviewComponent
    Introduces a new standalone MenuPreviewComponent with its HTML, SCSS, TypeScript, and test files. This component currently displays a placeholder and includes a basic test to verify creation.

---
+++ src/app/components/menu-preview/menu-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-preview.component.html',
+  styleUrl: './menu-preview.component.scss'
+})
+export class MenuPreviewComponent {
+
+}
