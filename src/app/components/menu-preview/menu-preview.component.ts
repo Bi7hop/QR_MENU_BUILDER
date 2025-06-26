de26a742e60cf585692e56a0e8330b4cc0f1762b
Marcel Menke <marcel.menke1981@gmail.com>
2025-06-26 09:24:32 +0200

    Add services and utility methods to MenuPreviewComponent
    Injected MenuService and ThemeService into MenuPreviewComponent, added methods for accessing restaurant data, menu categories, current theme, and utility functions for item counting and trackBy functions. Also implemented OnInit lifecycle hook.

--- src/app/components/menu-preview/menu-preview.component.ts
+++ src/app/components/menu-preview/menu-preview.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuService } from '../../services/menu.service';
+import { ThemeService } from '../../services/theme.service';
+import { MenuCategory, MenuItem, Restaurant } from '../../models/menu.models';
 
 @Component({
   selector: 'app-menu-preview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu-preview.component.html',
   styleUrl: './menu-preview.component.scss'
 })
-export class MenuPreviewComponent {
+export class MenuPreviewComponent implements OnInit {
+  public menuService = inject(MenuService);
+  public themeService = inject(ThemeService);
 
+  ngOnInit() {
+    // Komponente initialisiert
+  }
+
+  restaurant(): Restaurant {
+    return this.menuService.restaurant();
+  }
+
+  menuCategories() {
+    return this.menuService.menuCategories();
+  }
+
+  currentTheme() {
+    return this.themeService.getCurrentTheme();
+  }
+
+  getTotalItems(): number {
+    return this.menuCategories().reduce((total, category) => total + category.items.length, 0);
+  }
+
+  trackByCategory(index: number, category: MenuCategory): number {
+    return category.id;
+  }
+
+  trackByItem(index: number, item: MenuItem): number {
+    return item.id;
+  }
 }
